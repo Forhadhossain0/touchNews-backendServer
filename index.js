3219e222c9a8b77baec3473d127e3f9a74d50fad
forhadhossain <forhadhossainopc@gmail.com>
2023-11-09 13:08:49 +0600

    url updated fro fetching data

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin:[
     // 'http://localhost:5173',
     // 'http://localhost:5174',
-    'https://fotouch-project.web.app',
+    'https://fotouch-project.web.app', 
     'https://fotouch-project.firebaseapp.com'
   ],
   credentials:true
